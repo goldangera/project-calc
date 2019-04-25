f2a849558db2cf26bdcc0e71bc373fbd8d1f2706
goldangera <nkirotegolda6888@gmail.com>
2019-04-25 15:48:48 +0300

    file create

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var person = prompt("Give Name!")
+alert("hae "+ person) 
+var number1 = 13
+var number2 = 7
+function division(number1 ,number2) {
+return number1 / number2
+}
+alert (division(6,7).toExponential());
+ 
+
+
+var number5 = prompt("Input number")
+var number7 = prompt("Input another number")
+function checkEven(number5) {
+if (number5% 2===0){return ("is an even no.")}
+else {return("is not an even no.")}
+}
+
+alert(checkEven(number5));
